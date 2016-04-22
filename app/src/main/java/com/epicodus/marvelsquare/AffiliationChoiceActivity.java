3fc2393da6e6855809dada7bcbee8786bdf97d6f
Charles Carter <charles.h.carter@gmail.com>
2016-04-22 13:57:38 -0700

    integrating heroes into listview

--- app/src/main/java/com/epicodus/marvelsquare/AffiliationChoiceActivity.java
+++ app/src/main/java/com/epicodus/marvelsquare/AffiliationChoiceActivity.java
@@ -2,12 +2,19 @@ package com.epicodus.marvelsquare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class AffiliationChoiceActivity extends AppCompatActivity {
+    @Bind(R.id.evilAffilButton) Button mEvilAffilButton;
+    @Bind(R.id.goodAffilButton) Button mGoodAffilButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affiliation_choice);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/epicodus/marvelsquare/HeroChoiceActivity.java
+++ app/src/main/java/com/epicodus/marvelsquare/HeroChoiceActivity.java
@@ -2,12 +2,27 @@ package com.epicodus.marvelsquare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HeroChoiceActivity extends AppCompatActivity {
+    private ListView mHeroListView;
+    private String[] goodHeroListVal;
+    private String [] evilHeroListVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hero_choice);
+
+        mHeroListView = (ListView) findViewById(R.id.heroListView);
+        goodHeroListVal = getResources().getStringArray(R.array.good_heroes);
+        evilHeroListVal = getResources().getStringArray(R.array.evil_heroes);
+        ArrayAdapter goodAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, goodHeroListVal);
+        ArrayAdapter evilAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, evilHeroListVal);
+
+
     }
 }
