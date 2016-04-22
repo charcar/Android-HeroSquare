46bb12e1445526da4303d3fe9f69efe8b1e48cf6
Charles Carter <charles.h.carter@gmail.com>
2016-04-22 12:29:53 -0700

    add initial pathing to new activities

--- app/src/main/java/com/epicodus/marvelsquare/MainActivity.java
+++ app/src/main/java/com/epicodus/marvelsquare/MainActivity.java
@@ -1,13 +1,31 @@
 package com.epicodus.marvelsquare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.startChoiceButton) Button mStartChoiceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mStartChoiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AffiliationChoiceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
