9e7d385ab5daf83a6008590265006c815669d1d0
Charles Carter <charles.h.carter@gmail.com>
2016-04-22 11:47:32 -0700

    create four basic activities for user set up

---
+++ app/src/main/java/com/epicodus/marvelsquare/HeroChoiceActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.marvelsquare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeroChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hero_choice);
+    }
+}
---
+++ app/src/main/java/com/epicodus/marvelsquare/TeamChoiceActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.marvelsquare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeamChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_choice);
+    }
+}
