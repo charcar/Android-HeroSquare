1659f3cc7375ff5b2f69e4827b654fa6099454f8
charlie carter <charles.h.carter@gmail.com>
2016-05-04 16:43:09 -0700

    api call returning correct information

--- app/src/main/java/com/epicodus/marvelsquare/ComicvineService.java
+++ app/src/main/java/com/epicodus/marvelsquare/ComicvineService.java
@@ -1,6 +1,8 @@
 package com.epicodus.marvelsquare;
 
+import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -11,16 +13,22 @@ import okhttp3.Request;
 public class ComicvineService {
 
     public static void findCharacters(String name, Callback callback) {
+
         String COMICVINE_KEY = Constants.COMICVINE_KEY;
-        String COMICVINE_SEARCH_FILTER = Constants.COMICVINE_SEARCH_FILTER;
+        String COMICVINE_SEARCH_LIMIT = Constants.COMICVINE_SEARCH_LIMIT;
+        String COMICVINE_NAME_SIGNIFIER = Constants.COMICVINE_NAME_SIGNIFIER;
+        String COMICVINE_FORMAT = Constants.COMICVINE_FORMAT;
+
+
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .build();
 
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.COMICVINE_BASE_URL).newBuilder();
         urlBuilder.addQueryParameter("api_key", COMICVINE_KEY);
-        urlBuilder.fragment(Constants.COMICVINE_SEARCH_FILTER);
-        urlBuilder.addQueryParameter(Constants.COMICVINE_CHARACTER_QUERY_PARAMETER, name);
+        urlBuilder.addQueryParameter(COMICVINE_SEARCH_LIMIT, "15");
+        urlBuilder.addQueryParameter(COMICVINE_FORMAT, Constants.COMICVINE_JSON);
+        urlBuilder.addQueryParameter(Constants.COMICVINE_CHARACTER_QUERY_PARAMETER, COMICVINE_NAME_SIGNIFIER + name);
 
         String url = urlBuilder.build().toString();
         Log.v("Full URL PATH", url);
--- app/src/main/java/com/epicodus/marvelsquare/Constants.java
+++ app/src/main/java/com/epicodus/marvelsquare/Constants.java
@@ -4,7 +4,10 @@ package com.epicodus.marvelsquare;
 public class Constants {
     public static final String COMICVINE_KEY = BuildConfig.COMICVINE_KEY;
     public static final String COMICVINE_BASE_URL = "http://www.comicvine.com/api/characters/";
-    public static final String COMICVINE_SEARCH_FILTER = "&limit=15&format=json&filter=";
-    public static final String COMICVINE_CHARACTER_QUERY_PARAMETER = "name";
+    public static final String COMICVINE_SEARCH_LIMIT = "limit";
+    public static final String COMICVINE_FORMAT = "format";
+    public static final String COMICVINE_JSON = "json";
+    public static final String COMICVINE_CHARACTER_QUERY_PARAMETER = "filter";
+    public static final String COMICVINE_NAME_SIGNIFIER = "name:";
 
 }
